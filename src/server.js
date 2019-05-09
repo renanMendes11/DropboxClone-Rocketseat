72d78956f1220179e7ec9f4bbf3168001ed402ab
Renan <renanmendes049@gmail.com>
2019-05-09 01:28:20 -0300

    adiciono variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require('./routes'));
-server.listen(3333);
+server.listen(process.env.PORT ||  3333);
 
 
 
